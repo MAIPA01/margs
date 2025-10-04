6d270a0c770535537f53ad72c298233a57366468
MAIPA01 <marcelipatryk@gmail.com>
2025-10-04 20:34:18 +0200

    Some config fixes

--- include/margs/margs_config.hpp
+++ include/margs/margs_config.hpp
@@ -11,24 +11,35 @@
 #pragma region VERSION
 #define MARGS_VERSION_MAJOR 1
 #define MARGS_VERSION_MINOR 0
-#define MARGS_VERSION_PATCH 0
+#define MARGS_VERSION_PATCH 1
 
-#define MARGS_VERSION_TO_STRING(major, minor, patch) #major #minor #patch
-#define MARGS_VERSION_TO_INT(major, minor, patch) (major * 100 + minor * 10 + patch)
+#define _MARGS_STRINGIFY_HELPER(x) #x
 
-#define MARGS_VERSION_STRING MARGS_VERSION_TO_STRING(MARGS_VERSION_MAJOR, \
-												MARGS_VERSION_MINOR, \
-												MARGS_VERSION_PATCH)
-#define MARGS_VERSION_INT MARGS_VERSION_TO_INT(MARGS_VERSION_MAJOR, \
-											MARGS_VERSION_MINOR, \
-											MARGS_VERSION_PATCH)
+#define _MARGS_VERSION_TO_STRING(major, minor, patch) _MARGS_STRINGIFY_HELPER(major)"."\
+														_MARGS_STRINGIFY_HELPER(minor)"."\
+														_MARGS_STRINGIFY_HELPER(patch)
+#define _MARGS_VERSION_TO_INT(major, minor, patch) (major * 100 + minor * 10 + patch)
+
+#define MARGS_VERSION_STRING _MARGS_VERSION_TO_STRING(MARGS_VERSION_MAJOR, \
+															MARGS_VERSION_MINOR, \
+															MARGS_VERSION_PATCH)
+#define MARGS_VERSION_INT _MARGS_VERSION_TO_INT(MARGS_VERSION_MAJOR, \
+													MARGS_VERSION_MINOR, \
+													MARGS_VERSION_PATCH)
+
 #define MARGS_VERSION MARGS_VERSION_STRING
 #pragma endregion
 
 #pragma region LAST_UPDATE
-#define MARGS_LAST_UPDATE_DAY 13
-#define MARGS_LAST_UPDATE_MONTH 3
+#define MARGS_LAST_UPDATE_DAY 4
+#define MARGS_LAST_UPDATE_MONTH 10
 #define MARGS_LAST_UPDATE_YEAR 2025
 
-#define MARGS_LAST_UPDATE_DATE "13.03.2025"
+#define _MARGS_LAST_UPDATE_DATE_HELPER(day, month, year) _MARGS_STRINGIFY_HELPER(day)"."\
+															_MARGS_STRINGIFY_HELPER(month)"."\
+															_MARGS_STRINGIFY_HELPER(year)
+
+#define MARGS_LAST_UPDATE_DATE _MARGS_LAST_UPDATE_DATE_HELPER(MARGS_LAST_UPDATE_DAY, \
+																	MARGS_LAST_UPDATE_MONTH, \
+																	MARGS_LAST_UPDATE_YEAR)
 #pragma endregion
